92bdd93beef8f022a18831dc7325c57b76375039
S4mDev <karaban212111@yandex.ru>
2022-01-13 14:56:42 +0300

    commit

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,1 +1,12 @@
 import '../scss/main.scss';
+import 'slick-slider';
+
+$('.main-banner__slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    infinite: true,
+    speed: 2500,
+    arrows: true,
+    autoplay: true,
+    autoplaySpeed: 5000,
+});
